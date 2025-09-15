88c3e0742d290db75149b521b87997f60d59b77a
Leon Hosch <leon.hosch.6113@gmail.com>
2025-09-15 16:10:51 +0200

    Added logic so a base version can now be played as CLI

--- modules/question_manager.py
+++ modules/question_manager.py
@@ -2,14 +2,13 @@ import requests
 
 class QuestionManager:
 
-    def __init__(self, amount = 10, difficulty = None, category = None, type = None):
-        self.amount = amount
+    def __init__(self, difficulty = None, category = None, qtype = None):
+        self.AMOUNT = 1
         self.difficulty = difficulty
         self.category = category
-        self.type = type
+        self.type = qtype
         self.generate_token()
         self.generate_params()
-        self.get_data()
 
     def generate_token(self):
         self.token = requests.get("https://opentdb.com/api_token.php?command=request")
@@ -20,15 +19,15 @@ class QuestionManager:
             self.token = self.token["token"]
 
     def generate_params(self):
-        self.params = {"amount": self.amount, "token": self.token}
+        self.params = {"amount": self.AMOUNT, "token": self.token}
         if self.difficulty:
             self.params["difficulty"] = self.difficulty
         if self.category:
             self.params["category"] = self.category
         if self.type:
-            self.params["type"] = self.type
+            self.params["type"] = self.qtype
         
-    def get_data(self):
+    def get_question(self):
         API_URL = "https://opentdb.com/api.php"
         request = requests.get(API_URL, params=self.params)
         self.data = request.json()
